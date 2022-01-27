ed26cd55cc0abae1df3a25d29614ad7fdaa1fe20
Courtney Foster <cfoster649@gmail.com>
2022-01-26 21:35:32 -0800

    Set up basic prompts for manager

--- index.js
+++ index.js
@@ -1,3 +1,78 @@
+var fs = require('fs');
+
+var inquirer = require('inquirer');
+
+const validAns = 
+
+console.log("\n***\n Welcome to Team Profile generator! \nType your response to each prompt and press enter to submit. \n***\n")
+
+inquirer
+    .prompt([
+        {
+            message: 'Manager name:',
+            type: 'input',
+            name: 'man-name',
+            validate: input => {
+                if (input) {
+                    return true;
+                }
+                else {
+                    console.log("Please enter a name")
+                }
+            }
+        },
+        {
+            message: 'Manager ID:',
+            type: 'input',
+            name: 'man-id',
+            validate: input => {
+                if (input) {
+                    return true;
+                }
+                else {
+                    console.log("Please enter an ID")
+                }
+            }
+        },
+        {
+            message: 'Manager email address:',
+            type: 'input',
+            name: 'man-email',
+            validate: input => {
+                if (input) {
+                    return true;
+                }
+                else {
+                    console.log("Please enter an email")
+                }
+            }
+        },
+        {
+            message: 'Manager office number:',
+            type: 'input',
+            name: 'man-office',
+            validate: input => {
+                if (input) {
+                    return true;
+                }
+                else {
+                    console.log("Please enter an office number")
+                }
+            }
+        },
+        {
+            message: 'Choose next employee below:',
+            type: 'list',
+            name: 'add-emp',
+            choices: [
+                "Intern",
+                "Engineer",
+                "I am finished",
+            ]
+        }])
+.then((r) => {
+    console.log(r)
+})
 
 /*
 1 - create employee class
