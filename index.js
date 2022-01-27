434dbc2aa5c00638a20d1f39c220e9db6ba6a636
Courtney Foster <cfoster649@gmail.com>
2022-01-26 19:58:11 -0800

    psuedocode

---
+++ index.js
@@ -0,0 +1,10 @@
+
+/*
+1 - create employee class
+2 - create engineer, intern, manager, extension classes
+3 - create tests for each class
+4 - run tests for each class
+5 - create cli prompts
+6 - create html skeleton
+7 - run cli to make functioning html page
+*/
