fd406e456e4d8342dbbdf2c32322b8b9e4e28f97
Courtney Foster <cfoster649@gmail.com>
2022-01-27 19:47:50 -0800

    Validated engineer tests

--- _tests_/engineer.test.js
+++ _tests_/engineer.test.js
@@ -1,9 +1,33 @@
 const Engineer = require("../lib/engineer");
 
-describe("GitHub username", () => {
-    it("Should return the GitHub username entered into CLI", () => {
-        expect(new Engineer("bob, 32, bob@bob.com, bob123").toBe("bob123"));
+
+describe("Engineer values", () => {
+    it("Should give you back the name entered into the class", () => {
+        let newEmp = new Engineer("ed", "3", "ed@engineer.com", "edengineer");
+        let testName = newEmp.getName();
+    
+        expect(testName).toBe("ed")
     })
-})
+    
+    it("Should give you back the name entered into the class", () => {
+        let newEmp = new Engineer("ed", "3", "ed@engineer.com", "edengineer");
+        let testId = newEmp.getId();
+    
+        expect(testId).toBe("3")
+    })
+    
+    it("Should give you back the name entered into the class", () => {
+        let newEmp = new Engineer("ed", "3", "ed@engineer.com", "edengineer");
+        let testEmail = newEmp.getEmail();
+    
+        expect(testEmail).toBe("ed@engineer.com")
+    })
 
+    it("Should give you back the name entered into the class", () => {
+        let newEmp = new Engineer("ed", "3", "ed@engineer.com", "edengineer");
+        let testGH = newEmp.github;
+    
+        expect(testGH).toBe("edengineer")
+    })
 
+    })
